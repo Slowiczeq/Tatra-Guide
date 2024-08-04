6afd250baed82a7ead812c9062dd6350cd21f721
p.slowik <patrycjaaa319@gmail.com>
2024-08-04 21:13:51 +0200

    heroku fix

--- client/tatra-app/vite.config.js
+++ client/tatra-app/vite.config.js
@@ -15,9 +15,9 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: process.env.VITE_API_URL,
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+        secure: false,
       },
     },
   },
