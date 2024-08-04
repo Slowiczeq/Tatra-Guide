d7e97cf55d9ae6de0d32b5efbbab82470c6d9446
p.slowik <patrycjaaa319@gmail.com>
2024-08-04 16:44:10 +0200

    heroku app fix

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const cors = require("cors");
 const morgan = require("morgan");
 const SHA256 = require("crypto-js/sha256");
